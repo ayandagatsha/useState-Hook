692768cb40666baec7abab3eb8402883a5268db5
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-03-08 16:54:50 +0400

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App() {
-  return <div />;
-}
+  // const state = React.useState(3);
+  const [count, setCount] = React.useState(0);
 
+
+  function increase() {
+    // count++;
+setCount(count +1)
+    // console.log(state);
+  }
+
+  function decrease() {
+    // count++;
+setCount(count -1)
+    // console.log(state);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
 export default App;
